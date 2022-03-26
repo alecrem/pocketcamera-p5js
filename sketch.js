369526c64e6699a865a8179e3f472f1a03f41eed
Alejandro Cremades <ale@alecrem.com>
2022-03-26 15:17:35 +0900

    Parse image into matrix with int [0-3] per pixel

--- sketch.js
+++ sketch.js
@@ -1,43 +1,69 @@
 let img = [], imgIndex;
 let offsetX, offsetY, sqSide;
 const bgColor = 0;
+const pixelOverlap = 2;
+let photoPixels = [];
 
 function preload() {
   img.push(loadImage("./photos/20070804_pescao.png"));
 }
 
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+
   imgIndex = 0;
   img[imgIndex].loadPixels();
-  // console.log(img[imgIndex].get(0, img[imgIndex].height - 1));
-  createCanvas(windowWidth, windowHeight);
+  photoPixels = parseImage();
   windowResized();
-  noStroke();
   drawEverything();
-}
-
-function draw() {
-  // background(bgColor);
 }
 
 function drawEverything() {
   background(bgColor);
   push();
   translate(offsetX, offsetY);
-  // rect(0, 0, sqSide, sqSide);
   drawPhoto();
   pop();
 }
+
+function parseImage() {
+  const initialX = (img[imgIndex].width - img[imgIndex].height) / 2;
+  let allRowsPix = [];
+  for (y = 0; y < img[imgIndex].height; y++) {
+    let rowPix = [];
+    for (x = initialX; x < img[imgIndex].height + initialX; x++) {
+      let pix = img[imgIndex].get(x, y);
+      // 0, 102, 153, 255
+      if (pix[0] < 64) {
+        rowPix.push(0);
+      } else if (pix[0] < 128) {
+        rowPix.push(1);
+      } else if (pix[0] < 192) {
+        rowPix.push(2);
+      } else {
+        rowPix.push(3);
+      }
+    }
+    allRowsPix.push(rowPix);
+  }
+  return allRowsPix;
+}
+
 function drawPhoto() {
   const pixelSize = sqSide/img[imgIndex].height;
-  initialX = (img[imgIndex].width - img[imgIndex].height) / 2;
+  const initialX = (img[imgIndex].width - img[imgIndex].height) / 2;
   for (y = 0; y < img[imgIndex].height; y++) {
     for (x = initialX; x < img[imgIndex].height + initialX; x++) {
       let pix = img[imgIndex].get(x, y);
       fill(pix);
-      rect((x - initialX) * pixelSize, y * pixelSize, ~~pixelSize+2, ~~pixelSize+2);
+      rect((x - initialX) * pixelSize, y * pixelSize, ~~pixelSize + pixelOverlap, ~~pixelSize + pixelOverlap);
     }
   }
+  fill(bgColor);
+  fill(0);
+  rect(sqSide, 0, pixelOverlap, sqSide);
+  rect(0, sqSide, sqSide, pixelOverlap);
 }
 
 function windowResized() {
